b3bf8b308f53775696b01f183c3d829d09d4a952
DIOGO PATRICIO DONNER <diogo.donner@alunos.sc.senac.br>
2025-05-16 21:04:36 -0300

    Primeira Atividade Realizada

---
+++ index.js
@@ -0,0 +1,21 @@
+function AlertaBotao() {
+    console.log("Botão Clicado");
+    const botaoalarme = document.getElementById("botao_alarme");
+    botaoalarme.addEventListener("click",AlertaBotao);
+    alert("Olá");
+}
+
+function ExibirPalavra(){
+    const palavra = document.getElementById(palavra_)
+    const botaovalidar = document.getElementById(validar_botao)
+   
+    
+
+}
+
+function SiteFuncionamento()
+{
+    const botaoavalidar = document.getElementById(validar_botao);
+    validar_botao.addEventListener("click", )
+
+}
